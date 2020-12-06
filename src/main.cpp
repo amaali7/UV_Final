044c3e3b1664cd1a21bf0a96c90a741548506d61
Abdallah Mohammad Adam <amaali1991@gmail.com>
2020-12-06 09:23:39 +0200

    murge

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+
+
 #include "libs/webInterface.h"
 
 /****************************************************************\
@@ -9,11 +11,11 @@
     l1 = 13 , l2 = 12 , l3 = 14 , l4 = 27 , l5 = 25 , l6 = 26
 
       Groups :-
-    
+
     G1 = 23 , G2 = 4 , G3 = 2
 
       Motion Sensors :-
-    
+
     MS1 = 34 , MS2 = 39 , MS3 = 36
 
       CT Sensors :-
@@ -21,17 +23,17 @@
     CT1 = 32 , CT2 = 33 , CT3 = 35
 
       Main Lock :-
-    
+
     ML = 18
 
       Alarm :-
-    
+
     AL = 5
     SAL = 19
 
       RTC Module :-
 
-    SDA = 21 , SCL = 22  
+    SDA = 21 , SCL = 22
 
 \*****************************************************************/
 
@@ -60,7 +62,7 @@ void setup() {
   CTS1.calibrate();
   CTS2.calibrate();
   CTS3.calibrate();
-  
+
   // Configure BUZZER functionalities.
   ledcSetup(0, 5000, 12);
   // Attach BUZZER pin.
